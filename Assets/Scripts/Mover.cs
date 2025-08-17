135ce41118870b40436eeb884235ee9e620e6765
senymaster <pinkie2000@mail.ru>
2025-08-17 21:27:34 +0500

    Editing

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class Mover : MonoBehaviour
 {
     [SerializeField] private Vector3 _movementDirection;
 
